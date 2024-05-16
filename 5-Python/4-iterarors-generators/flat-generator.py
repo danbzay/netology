48a295dd8be253b46e7241a78c0a3bbb92e64768
danbzay <152090422+danbzay@users.noreply.github.com>
2024-05-16 07:46:06 +0300

    Add files via upload

--- 5-Python/4-iterarors-generators/flat-generator.py
+++ 5-Python/4-iterarors-generators/flat-generator.py
@@ -6,7 +6,6 @@ def flat_generator(list_of_lists):
         if isinstance(item, list):
             yield from flat_generator(item) 
         else:
-            print(f'{item = }')
             yield item
     
 
@@ -31,7 +30,25 @@ def test_2():
     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
 
+def test_4():
 
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
 
 if __name__ == '__main__':
     test_2()
+    test_4()
