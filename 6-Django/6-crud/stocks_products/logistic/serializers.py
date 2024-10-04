4de89e3770ea694698059262a9ae8a83d15742a3
Danil <danbzay@gmail.com>
2024-10-04 13:27:11 +0700

    6 hw done

---
+++ 6-Django/6-crud/stocks_products/logistic/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    #stock = StockSerializer()
+    #product = ProductSerializer()
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+        #depth = 2
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    class Meta:
+        model = Stock
+        fields = ['id', 'positions', 'address']
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        #stock_positions = [dict(_, **{'stock':stock}) for _ in positions]
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                    stock=stock, 
+                    product = position['product'], 
+                    defaults = position)
+
+        return stock
---
+++ 6-Django/6-crud/stocks_products/logistic/views.py
@@ -0,0 +1,21 @@
+from rest_framework.viewsets import ModelViewSet
+import django_filters.rest_framework 
+
+
+from logistic.models import Product, Stock, StockProduct
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['products']
+
