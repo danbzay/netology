ed4717188fdff19826c80fe5d591cd20be1e60bf
Danil <danbzay@gmail.com>
2024-10-13 00:03:29 +0700

    + search product

--- 6-Django/6-crud/stocks_products/logistic/views.py
+++ 6-Django/6-crud/stocks_products/logistic/views.py
@@ -9,6 +9,8 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
