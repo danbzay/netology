8de59cbef2bf1e50c74403f477185583ea77d982
danbzay <danbzay@gmail.com>
2025-09-01 01:55:18 +0700

    go home

--- 9-JavaScript-browser/2-DOM/hit-game/webpack.config.js
+++ 9-JavaScript-browser/2-DOM/hit-game/webpack.config.js
@@ -10,3 +10,17 @@ module.exports = {
   },
   plugins: [new HtmlWebpackPlugin()],
 };
+
+module: {
+  rules: [{
+    test: /\.(?:js|mjs|cjs)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        targets: "defaults",
+        presets: [['@babel/preset-env']]
+      }
+    }
+  }]
+}
